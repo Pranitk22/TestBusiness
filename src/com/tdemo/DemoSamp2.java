7abb3230c3886164b162054b7df97442d24ea51a
Spanu26 <anuradhapatil836@gmail.com>
2024-08-25 22:28:38 +0530

    This is my second testing business commit

---
+++ src/com/tdemo/DemoSamp2.java
@@ -0,0 +1,18 @@
+package com.tdemo;
+
+public class DemoSamp2 {
+
+	public static void main(String[] args) {
+		
+		int year = 2024;
+		
+		if(year%25 != 0) {
+			
+			System.out.println("Year: "+year+" is a Leap Year");
+			
+		} else {
+			
+			System.out.println("Year: "+year+" is not a Leap Year");
+		}
+	}
+}
