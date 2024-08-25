4d24a16ba3af0735283e48ba1236cbeaa5d9a35e
7pranit <pranitkonde007@gmail.com>
2024-08-25 21:39:57 +0530

    This is my first testing business commit

---
+++ src/com/tdemo/SampleDemo.java
@@ -0,0 +1,18 @@
+package com.tdemo;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		
+		int year = 2019;
+		
+		if(year%4 == 0) {
+			
+			System.out.println("Year: "+year+" is a Leap year");
+			
+		} else {
+			System.out.println("Year: "+year+" is not a leap year");
+			
+		}
+    }
+}
