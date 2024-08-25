5cd5fadd661fb85188fd4fedaed4c9f396ac7afa
7pranit <pranitkonde007@gmail.com>
2024-08-26 00:07:32 +0530

    This is my Third business commit

---
+++ src/com/tdemo/SampleTest1.java
@@ -0,0 +1,15 @@
+package com.tdemo;
+
+public class SampleTest1 {
+	
+	public static void main(String[] args) {
+		
+		int a = 18;
+		int b = 35;
+		
+		int c = a*b;
+		
+		System.out.println("Multiplication is: "+c);
+	}
+
+}
