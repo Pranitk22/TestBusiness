c6fc50a576be69d2d4cd8e83fcace5b88a12e402
Spanu26 <anuradhapatil836@gmail.com>
2024-08-25 22:42:17 +0530

    Requested changes are done successfully

--- src/com/tdemo/DemoSamp2.java
+++ src/com/tdemo/DemoSamp2.java
@@ -4,9 +4,9 @@ public class DemoSamp2 {
 
 	public static void main(String[] args) {
 		
-		int year = 2024;
+		int year = 2020;
 		
-		if(year%25 != 0) {
+		if(year%4 == 0) {
 			
 			System.out.println("Year: "+year+" is a Leap Year");
 			
